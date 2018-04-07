68a44e4d7e9ec40b9ca69d867c7ff2a8b3d345f3
CastellierGwenole <castellier.gwenole@gmail.com>
2018-04-07 16:50:45 +0200

    finish

--- style.js
+++ style.js
@@ -1,29 +1,87 @@
 
+verifCo()
 function recupName() {
-let name = '';
-var nameRecup = document.getElementById("recup").value
-db.collection("personnages").add({
-  name: nameRecup
-
+  let name = '';
+  var nameRecup = document.getElementById("recup").value
+  db.collection("personnages").add({
+    name: nameRecup
   
-})
+    
+  })
+  
+  db.collection("personnages").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+       name = name + '<br> ' + doc.data().name;
+        document.getElementById("listPerso").innerHTML = `<p>${name}</p>`;
+        console.log(`${doc.data().name}`);
+    });
+  })
+  }
+  let init = '';
+  db.collection("personnages").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      init = init + '<br> ' + doc.data().name;
+        document.getElementById("listPerso").innerHTML = `<p>${init}</p>`;
+        console.log(`${doc.data().init}`);
+    });
+  })
+  
+  
+  function createCompte(){
+    let address = document.getElementById("addressmailsignout").value
+    let mdp = document.getElementById("mdpsignout").value
+    
+    firebase.auth().createUserWithEmailAndPassword(address, mdp).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  }
 
-db.collection("personnages").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-     name = name + '<br> ' + doc.data().name;
-      document.getElementById("listPerso").innerHTML = `<p>${name}</p>`;
-      console.log(`${doc.data().name}`);
+  function connection(){
+    
+    let address = document.getElementById("addressmailsignin").value
+    let mdp = document.getElementById("mdpsignin").value
+
+    firebase.auth().signInWithEmailAndPassword(address, mdp).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+  }
+  
+  document.getElementById("modif").style.display="none";
+function verifCo(){
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      document.getElementById("register").style.display="none";
+      document.getElementById("modif").style.display="block";
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+      console.log("Connecté(e)  " + email);
+    } else {
+      document.getElementById("register").style.display="block";
+      document.getElementById("modif").style.display="none";
+      console.log("non connecté");
+    }
   });
-})
 }
-let init = '';
-db.collection("personnages").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-    init = init + '<br> ' + doc.data().name;
-      document.getElementById("listPerso").innerHTML = `<p>${init}</p>`;
-      console.log(`${doc.data().init}`);
-  });
-})
 
 
-
+  function partir() {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
+    verifCo()
+  }
+  
